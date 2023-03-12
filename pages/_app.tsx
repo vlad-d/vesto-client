8f634cef716097c21f2674ade9e8bb144c3794b5
Razvan Statescu <razvan@statescu.net>
2023-03-12 11:54:13 +0200

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,22 @@
-import '../styles/globals.css'
-import type {AppProps} from 'next/app'
-import store from "../redux/store";
-import {Provider as ReduxProvider} from "react-redux";
-import Notifications from "../components/Notifications";
-import {AuthContextProvider} from "@elrond-giants/erd-react-hooks";
-import "react-datepicker/dist/react-datepicker.css";
+import '../styles/globals.css';
+import 'react-datepicker/dist/react-datepicker.css';
 
-function MyApp({Component, pageProps}: AppProps) {
-    return (
-        <ReduxProvider store={store}>
-            <AuthContextProvider
-                env={process.env.NODE_ENV === "production" ? "mainnet" : "devnet"}
-                projectId={process.env.NEXT_PUBLIC_WALLET_CONNECT_ID}
-            >
-                <Component {...pageProps} />
-                <Notifications/>
-            </AuthContextProvider>
-        </ReduxProvider>
-    );
+import { AuthContextProvider } from '@elrond-giants/erd-react-hooks';
+import { Provider as ReduxProvider } from 'react-redux';
+
+import Notifications from '../components/Notifications';
+import store from '../redux/store';
+
+import type { AppProps } from "next/app";
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ReduxProvider store={store}>
+      <AuthContextProvider env="devnet" projectId={process.env.NEXT_PUBLIC_WALLET_CONNECT_ID}>
+        <Component {...pageProps} />
+        <Notifications />
+      </AuthContextProvider>
+    </ReduxProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
